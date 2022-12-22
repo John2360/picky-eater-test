2b14280b9de1f268063bc8ed40c3c6b468c7626b
john <jfinberg@haverford.edu>
2022-12-21 18:36:19 -0800

    fix

--- src/App.js
+++ src/App.js
@@ -16,27 +16,27 @@ function App() {
             <h1 style={{textAlign: 'center'}}>Results</h1>
             <p>The results from the test indicate that the user is <b>100% a picky eater</b>. Thanks for using JF testing services.</p>
           </div>
-          </div>
+        </div>
         <div className="banner">
           <h1>How picky of an eater is Olivia?</h1>
         </div>
         <p>Select the items you will not eat and them click submit to check your eating status.</p>
         <form onSubmit={handleSubmit}>
-        <div class="line">
-          <input type="checkbox" class="my-checkbox" value="1" />
-          Uni
-        </div>
-        <div class="line">
-          <input type="checkbox" class="my-checkbox" value="1" />
-          Olives
-        </div>
-        <div class="line">
-          <input type="checkbox" class="my-checkbox" value="1" />
-          Sliced Bread
-        </div>
-        <button className='button' onClick={() => {setSubmit(true);}}>
-          Submit
-        </button>
+          <div className="line">
+            <input type="checkbox" className="my-checkbox" value="1" />
+            Uni
+          </div>
+          <div className="line">
+            <input type="checkbox" className="my-checkbox" value="1" />
+            Olives
+          </div>
+          <div className="line">
+            <input type="checkbox" className="my-checkbox" value="1" />
+            Sliced Bread
+          </div>
+          <button className='button' onClick={() => {setSubmit(true);}}>
+            Submit
+          </button>
         </form>
     </div>
   );
